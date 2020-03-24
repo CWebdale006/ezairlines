3353e586b92a07345ea1db73c9f7434764623210
connor <cwebdale006@west-mec.org>
2020-03-24 00:26:24 -0700

    progress
    i have made progress for the first time in a very very long time 
    uh 
    a lot of it works, my problem now is that auth0 doesn't like the body i'm sending, even though it's written correctly in JSON so idk what to do

--- ezairlines/src/components/UpdateInfo.js
+++ ezairlines/src/components/UpdateInfo.js
@@ -1,11 +1,36 @@
 import React from "react";
+import axios from 'axios';
 import { useAuth0 } from "../react-auth0-spa";
+
+// getting the body parser object
+let bodyParser = require('body-parser');
 
 export default function UpdateInfo() {
 
     const update = async () => {
         try {
+            /**
+             * getting a token to make requests to the management
+             * api with 
+             */
+            const response = await fetch("http://localhost:3001/get-token");
+            const responseData = await response.json();
+            const token = await responseData.token;
+
+            /** 
+             * now using the token
+             */
+            const info = {
+                userId: "google-oauth2|116658177472204313093",
+                token: token,
+                body: {
+                    // "user_metadata" : { "tickets": {BookedTickets} }
+                    'user_metadata': { 'test': 'from update info' }
+                }
+            };
             
+            axios.patch('http://localhost:3001/book', info)
+                .then(res=>console.log(res.data));
         } catch (error) {
             console.error("other error is: "+error);
         }
--- ezairlines/src/server.js
+++ ezairlines/src/server.js
@@ -3,11 +3,17 @@ const cors = require('cors');
 const jwt = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
 
+// .env
+require('dotenv').config();
+
 // creates a new express app 
 const app = express();
 
 // accept cross-origin requests from the frontend app 
 app.use(cors({ origin: 'http://localhost:3000' }));
+
+// body parser middleware  
+app.use(express.json());
 
 // set up auth0 configuration
 const authConfig = {
@@ -36,6 +42,63 @@ app.get('/api/external', checkJwt, (req, res) => {
         msg: 'Your access token was successfully validated sir'
     });
 });
+
+/**
+ * getting a token!!!!!!
+ */ 
+// hiding client secret
+const clientSecret = process.env.CLIENT_SECRET;
+
+// defining a path that our front-end can use 
+app.get('/get-token', (req, res)=>{
+    // doing what (i think) auth0 wants me to do, making a POST request 
+    // to my site and then returning the body, which has my token 
+    var request = require("request");
+
+    var options = { 
+        method: 'POST',
+        url: 'https://dev-0anjj2er.auth0.com/oauth/token',
+        headers: { 'content-type': 'application/json' },
+        body: '{"client_id":"Nye1Ijo0IA8fAQ4SXINaoq2owdY3h5u2","client_secret":"'+clientSecret+'","audience":"https://dev-0anjj2er.auth0.com/api/v2/","grant_type":"client_credentials"}'
+    };
+    
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+
+        res.send({
+            token: JSON.parse(body).access_token
+        });
+    });
+});
+
+/**
+ * updating the user through auth0
+ */
+app.patch('/book', (req, res)=>{
+    var request = require("request");
+
+    // breaking down request data
+    const userId = req.body.userId;
+    const token = req.body.token;
+    const body = req.body.body;
+
+    var options = {
+        method: 'PATCH',
+        url: 'https://dev-0anjj2er.auth0.com/api/v2/users/'+userId,
+        headers: {
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify(body)
+    }
+    console.log(options.body);
+
+    request(options, function(error, response, body) {
+        if (error) throw new Error(error);
+
+        res.json(response);
+    });
+});
+
 
 // starts the app 
 app.listen(3001, () => console.log('API listening on port 3001'));

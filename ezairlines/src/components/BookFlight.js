814bc81b928d49594ddd4f6bfdf2fd78b1b2d758
connor <cwebdale006@west-mec.org>
2020-03-27 16:20:23 -0700

    all function components ig
    i need to work with all functions components so i can access the props that i need to access 
    
    haha guess what, it works. 
    I worked with the fake state thing for function components and... yeah. I used the basic Update User function that I already had, and went from there

--- ezairlines/src/components/BookFlight.js
+++ ezairlines/src/components/BookFlight.js
@@ -1,55 +1,91 @@
-import React, { Component } from 'react';
-import { Router } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
-// class component to set state to ticket data 
-class TicketData extends Component {
-    constructor(props) {
-        super(props);
-
-        // not sure if this is actually needed, kept *for now*
-        this.state = {
-            departDate: new Date(),
-            returnDate: new Date()
-        }
-    }
-
-    componentDidMount() {
-        console.log(this);
-        // axios.get('http://localhost:3001/destinations/'+this.props.match.params.id)
-        //     .then(res=>{
-        //         this.setState({
-        //             from: res.data.from, 
-        //             to: res.data.to
-        //         })
-        //     })
-        //     .catch((error)=>{
-        //         console.log("componentDidMount axios error: "+error);
-        //     })
-    }
-
-    render() {
-        return(
-            <div>
-                hi
-            </div>
-        )
-    }
-}
+import { useAuth0 } from "../react-auth0-spa";
 
 // const TicketData = () => {
+//     // settting up variables to work with state 
+//     // const [ from, setFrom ] = useState();
+//     // const [ to, setTo ] = useState();
+//     // const [ departDate, setDepartDate ] = useState();
+//     // const [ returnDate, setReturnDate ] = useState();
+//     // const [ price, setPrice ] = useState();
+
+//     // getting id from parameters to use for axios request to database 
 //     let { id } = useParams();
-//     console.log(id);
+
+//     return (
+//         <div>
+//             he
+//         </div>
+//     )
 // }
 
 export default function BookFlight() {
+    const { user } = useAuth0();
+
+    // settting up variables to work with state 
+    const [ from, setFrom ] = useState();
+    const [ to, setTo ] = useState();
+    const [ departDate, setDepartDate ] = useState();
+    const [ returnDate, setReturnDate ] = useState();
+    const [ price, setPrice ] = useState();
+    const [ amount, setAmount ] = useState();
+    
+
+    // getting id from parameters to use for axios request to database 
+    let { id } = useParams();
+
+    axios.get('http://localhost:3001/destinations/'+id)
+        .then(res=>{
+            setFrom(res.data.from);
+            setTo(res.data.to);
+            setDepartDate(res.data.departDate);
+            setReturnDate(res.data.returnDate);
+            setPrice(res.data.price);
+        })
+        .catch((error)=>{
+            console.log("axios error: "+error);
+        })
+
     return (
         <>
-            <TicketData />
-            <div>
-                <h4>???</h4>
-            </div>
+            <button onClick={updateUser}>CLick</button>
         </>
     )
+
+    async function updateUser() {
+        try { 
+            // getting a token to make requests to the management 
+            // api with 
+            const response = await fetch("http://localhost:3001/get-token");
+            const responseData = await response.json();
+            const token = await responseData.token;
+
+            // creating the body to send 
+            const bookedTickets = {
+                from: from, 
+                to: to, 
+                departDate: departDate,
+                returnDate: returnDate,
+                price: price,
+                amount: amount
+            }
+
+            // using that token for a request
+            const info = { 
+                userId: user.sub, 
+                token: token, 
+                body: { 
+                    "user_metadata": { "tickets": { bookedTickets } }
+                }
+            }
+
+            axios.patch('http://localhost:3001/book', info)
+                .then(res=>console.log(res.data));
+        } catch (error) {
+            console.log("can't update user: "+error);
+        }
+    }
 }

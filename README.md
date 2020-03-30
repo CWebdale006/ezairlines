74e98b10b610da299720ddef9d91364b1aa0e97f
connor <cwebdale006@west-mec.org>
2020-03-30 04:43:46 -0700

    updates to booking tickets
    added a loading spinner when the tickets are being booked, and finished displaying the data from the ticket when the user is booking. the dates display correctly, as well as being able to pick the amount of tickets and see the price

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
         * [ ] - finish the weather function, try to get it to change which city it displays depending on which ticket is hovered over 
         * [ ] - google maps api was only added to meet project requirements for the final, but if i can get it to work that's cool too 
         * [ ] - i don't like the way the "Book a flight" changes to "Welcome, User!" everytime the page is refreshed, looks choppy, maybe add different cases for !user and loading? 
-        * [ ] - if the username for the account is missing, change the text that is displayed please (github account)
+        * [ x ] - if the username for the account is missing, change the text that is displayed please (github account)
     * [ ] - App.js -> App.js
         * [ ] - possibly need to add routes later
         * [ ] - uncomment the SearchDestination and Footer components, and actually write them 
@@ -20,3 +20,7 @@
     * [ ] - edit-destination.component.js -> BookFlight.js
       * [ ] - IMPORTANT!!!! the way the data is getting sent will make it awkward if a user books multiple tickets, so... fix that. idk how yet, but it should be changed 
       * [ ] - check your updateUser function to see if you can reduce the amount of variables used for cleaner code 
+* [ ] - 2 most urgent things: 
+    * [ ] - booking tickets needs to ADD to data, not REPLACE
+    * [ ] - display booked tickets on user profile 
+* [ ] - on the profile page, add animations from: https://daneden.github.io/animate.css/
--- ezairlines/src/components/BookFlight.js
+++ ezairlines/src/components/BookFlight.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 import axios from 'axios';
+import { css } from '@emotion/core';
+import ClipLoader from 'react-spinners/ClipLoader';
 
 import { useAuth0 } from "../react-auth0-spa";
 
@@ -14,26 +17,83 @@ export default function BookFlight() {
     const [ departDate, setDepartDate ] = useState(new Date());
     const [ returnDate, setReturnDate ] = useState(new Date());
     const [ price, setPrice ] = useState();
-    const [ amount, setAmount ] = useState();
+    const [ amount, setAmount ] = useState(1);
+
+    // css of the loader
+    const override = css`
+        display: block;
+        margin: 0 auto;
+        border-color: #0062cc;
+    `;
+
+    const overlay = {
+        position: "fixed",
+        display: "none",
+        width: "100%",
+        height: "100%",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: "rgba(0,0,0,0.85)",
+        zIndex: 2,
+        cursor: "pointer"
+    };
+
+    const text = {
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        fontSize: "50px",
+        color: "white",
+        transform: "translate(-50%,-50%)"
+    };
     
 
     // getting id from parameters to use for axios request to database 
     let { id } = useParams();
 
-    axios.get('http://localhost:3001/destinations/'+id)
-        .then(res=>{
-            setFrom(res.data.from);
-            setTo(res.data.to);
-            setDepartDate(new Date(res.data.departDate));
-            setReturnDate(new Date(res.data.returnDate));
-            setPrice(res.data.price);
-        })
-        .catch((error)=>{
-            console.log("axios error: "+error);
-        })
+    // useeffect takes a second parameter, putting it in fixes a 
+    // problem that i've been dealing with for a while, and makes it 
+    // so that the function only runs once, without it the axios request 
+    // get made over and over and breaks the site 
+    useEffect(()=>{
+        document.getElementById("overlay").style.display = "none"
+        axios.get('http://localhost:3001/destinations/'+id)
+            .then(res=>{
+                // setting state to data from my backend
+                setFrom(res.data.from);
+                setTo(res.data.to);
+                setDepartDate(new Date(res.data.departDate));
+                setReturnDate(new Date(res.data.returnDate));
+                setPrice(res.data.price);
+            })
+            .catch((error)=>{
+                console.log("axios error: "+error);
+            })
+    }, []);
+
+    const on = () => {
+        document.getElementById("overlay").style.display = "block"
+    }
+
+    const off = () => {
+        document.getElementById("overlay").style.display = "none"
+        window.location.replace("http://localhost:3000")
+    }
 
     return (
         <>
+            <div id="overlay" style={overlay} onClick={off}>
+                <div id="text" style={text}>
+                    <ClipLoader 
+                        css={override}
+                        size={100}
+                        color={"#0062cc"}
+                        loading={true}
+                    />
+                </div>
+            </div>
             <div className="container">
                 <div className="row">
                     <div className="col"></div>
@@ -42,7 +102,6 @@ export default function BookFlight() {
                         <div className="card">
                             <div className="card-body">
                                 {/* <h5 className="card-title">Special title treatment</h5> */}
-                                <p className="card-text">from, to, depart date, return date, price, and user-selected amount.</p>
 
                                 <div className="row">
                                     <div className="col">
@@ -61,30 +120,38 @@ export default function BookFlight() {
                                         <h6 className="card-text">Depart date: </h6>
                                     </div>
                                     <div className="col">
-                                        {/* <DatePicker 
-                                            selected={JSON.stringify(departDate)} 
-                                            onChange={date=>setDepartDate(date)} 
-                                        /> */}
+                                        <DatePicker 
+                                            selected={departDate}
+                                            onChange={date=>setDepartDate(date)}
+                                        />
                                     </div>
                                 </div>
                                 <div className="row">
                                     <div className="col">
                                         <h6 className="card-text">Return date: </h6>
                                     </div>
-                                    <div className="col">{JSON.stringify(returnDate)}</div>
+                                    {/* <div className="col">{JSON.stringify(returnDate)}</div> */}
+                                    <div className="col">
+                                        <DatePicker 
+                                            selected={returnDate}
+                                            onChange={date=>setReturnDate(date)}
+                                        />
+                                    </div>
                                 </div>
                                 <div className="row">
                                     <div className="col">
                                         <h6 className="card-text">Amount: </h6>
                                     </div>
-                                    <div className="col"></div>
+                                    <div className="col">
+                                        <input type="number" defaultValue="1" min="1" max="30" onChange={(e)=>{setAmount(e.target.value)}}/>
+                                    </div>
                                 </div>
                                 <div className="row">
                                     <div className="col">
                                         <h6 className="card-text">Price: </h6>
                                     </div>
                                     <div className="col">{
-                                        price*amount
+                                        (price*amount).toFixed(2)
                                     }</div>
                                 </div>
                                 <div className="row">
@@ -109,6 +176,9 @@ export default function BookFlight() {
 
     async function updateUser() {
         try { 
+            // starting the loading screen 
+            on()
+
             // getting a token to make requests to the management 
             // api with 
             const response = await fetch("http://localhost:3001/get-token");
@@ -121,23 +191,60 @@ export default function BookFlight() {
                 to: to, 
                 departDate: departDate,
                 returnDate: returnDate,
-                price: price,
+                pricePer: price,
+                priceTotal: (price*amount).toFixed(2),
                 amount: amount
             }
+
+            const asdf = "random string";
 
             // using that token for a request
             const info = { 
                 userId: user.sub, 
                 token: token, 
                 body: { 
-                    "user_metadata": { "tickets": { bookedTickets } }
+                    "user_metadata": { tickets: { bookedTickets } }
                 }
             }
 
             axios.patch('http://localhost:3001/book', info)
-                .then(res=>console.log(res.data));
+                .then(res=>{
+                    if (res.data.statusCode===400) {
+                        console.log(res.data);
+                        return (
+                            document.getElementById("text").innerHTML = `
+                                <p class="text-center">
+                                    Something went wrong :( <br/>
+                                    <small class="text-muted">please try again later</small>
+                                </p>
+                            `
+                        )
+                    }
+                    console.log(res.data);
+                    document.getElementById("text").innerHTML = `
+                        <p class="text-center">
+                            Successfully booked!<br/>
+                            <small class="text-muted">click to dismiss</small>
+                        </p>
+                    `;
+                })
+                .catch((error)=>{
+                    console.log("axios error when booking: "+error);
+                    document.getElementById("text").innerHTML = `
+                        <p class="text-center">
+                            Something went wrong :( <br/>
+                            <small class="text-muted">please try again later</small>
+                        </p>
+                    `;
+                })
         } catch (error) {
             console.log("can't update user: "+error);
+            document.getElementById("text").innerHTML = `
+                <p class="text-center">
+                    Something went wrong :( <br/>
+                    <small class="text-muted">please try again later</small>
+                </p>
+            `;
         }
     }
 }
